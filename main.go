ddb1e87cedb04463030007cb6598920122dac410
지농 <jinhong0719@naver.com>
2019-12-24 14:10:36 +0900

    API 라우팅 메서드 호출 후 서버 구동

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package ScienceQuiz_Backend
+// https://github.com/xesina/golang-echo-realworld-example-app
+package main
+
+import (
+	"github.com/ScienceQuiz-Backend/db"
+	"github.com/ScienceQuiz-Backend/handler"
+	"github.com/ScienceQuiz-Backend/route"
+	"github.com/ScienceQuiz-Backend/service"
+)
+
+func main() {
+	r := route.New()
+	v1 := r.Group("api")
+
+	d := db.New()
+	db.AutoMigrate(d)
+
+	u := service.NewUserService(d)
+	h := handler.NewHandler(u)
+
+	h.Register(v1)
+	r.Logger.Fatal(r.Start(":8080"))
+}
