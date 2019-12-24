d704297f3a53bcdef0866cd73d254ac707b6f586
지농 <jinhong0719@naver.com>
2019-12-24 11:00:49 +0900

    [CREATE] <model/user.go> User model 생성

--- model/user.go
+++ model/user.go
@@ -1,1 +1,12 @@
 package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	gorm.Model
+	UserId   string `gorm:"unique;not null"`
+	UserPw   string `gorm:"not null"`
+	Nickname string `gorm:"not null"`
+}
