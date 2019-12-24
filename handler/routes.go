c91fdcd63e539ea7557ba99fb95a3974c33d8be7
지농 <jinhong0719@naver.com>
2019-12-24 12:19:09 +0900

    첫 라우팅 성공띠

--- handler/routes.go
+++ handler/routes.go
@@ -1,1 +1,10 @@
 package handler
+
+import (
+	"github.com/labstack/echo"
+)
+
+func (h *Handler) Register(v1 *echo.Group) {
+	guestUser := v1.Group("/users")
+	guestUser.POST("/login", h.SignUp)
+}
