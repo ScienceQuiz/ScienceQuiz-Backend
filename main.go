f3fd1e9aacccc529bbe67187f9b5f8c7942bb8da
지농 <jinhong0719@naver.com>
2019-12-24 11:00:16 +0900

    [MODIFY] <route/router.go> CORS 설정 변경

---
+++ main.go
@@ -0,0 +1,1 @@
+package ScienceQuiz_Backend
--- route/router.go
+++ route/router.go
@@ -5,13 +5,13 @@ import (
 	echoMW "github.com/labstack/echo/middleware"
 )
 
-func Init() *echo.Echo {
+func New() *echo.Echo {
 	e := echo.New()
 
 	e.Use(echoMW.Logger())
 	e.Use(echoMW.CORSWithConfig(echoMW.CORSConfig{
 		AllowOrigins:     []string{"*"},
-		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAcceptEncoding},
+		AllowHeaders: 	  []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 	}))
 
 	return e
