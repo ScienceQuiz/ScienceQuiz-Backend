5137bf1a1accb2239e05f09b7702f3fb886627b5
지농 <jinhong0719@naver.com>
2019-12-24 12:21:02 +0900

    User Service에 필요한 메서드 interface로 정의

---
+++ service/interface/user.go
@@ -0,0 +1,10 @@
+package _interface
+
+import "github.com/ScienceQuiz-Backend/model"
+
+type User interface {
+	// 회원가입 API
+	Create(*model.User) error
+
+	CheckUserExist(*model.User) (bool, error)
+}
