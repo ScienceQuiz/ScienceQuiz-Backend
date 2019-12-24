0e46c06bb5527ba58a90a3bacb3204f73acf3b64
지농 <jinhong0719@naver.com>
2019-12-24 12:28:06 +0900

    모든 service 객체를 하나로 묶은 구조체 정의

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,15 @@
 package handler
+
+import (
+	"github.com/ScienceQuiz-Backend/service/interface"
+)
+
+type Handler struct {
+	userService _interface.User
+}
+
+func NewHandler(u _interface.User) *Handler{
+	return &Handler{
+		userService: u,
+	}
+}
