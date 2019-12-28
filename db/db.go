cf5715bdb1d66569ff5d7d4a8e399dd3c466a080
지농 <jinhong0719@naver.com>
2019-12-28 21:08:51 +0900

    Mysql 서버 접속 오류 수정

--- db/db.go
+++ db/db.go
@@ -1,20 +1,17 @@
 package db
 
 import (
+	"fmt"
 	"github.com/ScienceQuiz-Backend/model"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"log"
 )
 
 func New() *gorm.DB {
-	db, err := gorm.Open("mysql", "user:105212as@#@(localhost)/ScienceQuiz?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:105212as@#@tcp(localhost)/ScienceQuiz?charset=utf8&parseTime=True&loc=Local")
 
 	if err!=nil {
-		log.Println("Connection Failed to Open")
-	} else {
-		log.Println("Connection Established")
-		defer db.Close()
+		fmt.Println("storage err: ", err)
 	}
 
 	return db
