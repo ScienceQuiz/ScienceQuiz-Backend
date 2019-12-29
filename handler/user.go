803afdee88231645de8e10e6bfa2dc4f27abf005
지농 <jinhong0719@naver.com>
2019-12-29 23:48:28 +0900

    회원가입 API 구현 완료

--- handler/user.go
+++ handler/user.go
@@ -14,6 +14,10 @@ func(h *Handler) SignUp(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest)
 	}
 
+	if h.userService.CheckUserExist(&u) == true {
+		return echo.NewHTTPError(470, "해당 id의 사용자가 이미 존재합니다.")
+	}
+
 	h.userService.Create(&u)
 	return c.NoContent(http.StatusCreated)
 }
