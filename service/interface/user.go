34bbdadcbf27cf7327c81cc5e059cdc31fb8e10f
지농 <jinhong0719@naver.com>
2019-12-29 23:18:18 +0900

    모든 Service Method의 반환형에서 error 제외

--- service/interface/user.go
+++ service/interface/user.go
@@ -3,6 +3,8 @@ package _interface
 import "github.com/ScienceQuiz-Backend/model"
 
 type User interface {
-	// 회원가입 API
-	Create(*model.User) error
+	// 새 사용자 생성 메서드
+	Create(*model.User)
+	// (userId 기준) 사용자 존재 여부 확인 메서드
+	CheckUserExist(*model.User) bool
 }
