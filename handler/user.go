288edcc66c25b5818fe51a136c1a1b87f5c0317b
지농 <jinhong0719@naver.com>
2019-12-29 23:33:33 +0900

    데이터 유효성 검사만 추가한 회원가입

--- handler/user.go
+++ handler/user.go
@@ -1,10 +1,19 @@
 package handler
 
 import (
+	"github.com/ScienceQuiz-Backend/model"
 	"github.com/labstack/echo"
 	"net/http"
 )
 
 func(h *Handler) SignUp(c echo.Context) error {
-	return c.NoContent(http.StatusOK)
+	var u model.User
+	req := &userSignUpRequest{}
+
+	if err := req.bind(c, &u); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest)
+	}
+
+	h.userService.Create(&u)
+	return c.NoContent(http.StatusCreated)
 }
