b6eaede5a0a108ad9ae297c80800e9d0addcd579
지농 <jinhong0719@naver.com>
2019-12-29 21:44:35 +0900

    main 함수에서 에러 처리를 위한 반환값의 error 타입 추가

--- db/db.go
+++ db/db.go
@@ -7,14 +7,15 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func New() *gorm.DB {
+func New() (*gorm.DB, error) {
 	db, err := gorm.Open("mysql", "root:105212as@#@tcp(localhost)/ScienceQuiz?charset=utf8&parseTime=True&loc=Local")
 
 	if err!=nil {
 		fmt.Println("storage err: ", err)
+		return nil, err
 	}
 
-	return db
+	return db, nil
 }
 
 func AutoMigrate(db *gorm.DB) {
