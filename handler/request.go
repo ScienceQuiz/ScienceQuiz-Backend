813ed988121ca6924d63ef4a5f7d455fe6409a81
지농 <jinhong0719@naver.com>
2019-12-29 23:28:27 +0900

    회원가입 요청용 bind 메서드 정의

--- handler/request.go
+++ handler/request.go
@@ -1,7 +1,28 @@
 package handler
 
+import (
+	"github.com/ScienceQuiz-Backend/model"
+	"github.com/labstack/echo"
+)
+
 type userSignUpRequest struct {
 	UserId string `json:"userId" validate:"required"`
 	UserPw string `json:"userPw" validate:"required"`
 	Nickname string `json:"nickname" validate:"required"`
+}
+
+
+func (r *userSignUpRequest) bind(c echo.Context, u *model.User) error {
+	if err := c.Bind(r); err != nil {
+		return err
+	}
+	if err := c.Validate(r); err != nil {
+		return err
+	}
+
+	u.UserId = r.UserId
+	u.UserPw = r.UserPw // hash 적용 추가
+	u.Nickname = r.Nickname
+	
+	return nil
 }
