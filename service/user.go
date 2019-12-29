376ee7ee0e9414bd3183efd10c338b60222f0a85
지농 <jinhong0719@naver.com>
2019-12-29 23:48:03 +0900

    사용자 중복 체크 method 구현

--- service/user.go
+++ service/user.go
@@ -5,16 +5,23 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-type UserStore struct {
+type UserService struct {
 	db *gorm.DB
 }
 
-func NewUserService(db *gorm.DB) *UserStore {
-	return &UserStore{
+func NewUserService(db *gorm.DB) *UserService {
+	return &UserService{
 		db: db,
 	}
 }
 
-func (us *UserStore) Create(u *model.User) error {
-	return us.db.Create(u).Error
+func (us *UserService) Create(u *model.User) {
+	us.db.Create(u)
+}
+
+func (us *UserService) CheckUserExist(u *model.User) bool {
+	if us.db.Where("user_id = ?", u.UserId).Find(&u).RowsAffected != 0 {
+		return true
+	}
+	return false
 }
