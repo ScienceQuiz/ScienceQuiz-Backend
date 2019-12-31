97a0486a1e33bc34d9f063595d0d8307803a077b
지농 <jinhong0719@naver.com>
2019-12-31 12:05:39 +0900

    jwt 반환용 객체 생성 및 초기화 후 반환 메서드 생성

--- handler/response.go
+++ handler/response.go
@@ -1,5 +1,14 @@
 package handler
 
+import "github.com/ScienceQuiz-Backend/utils"
+
 type jwtResponse struct {
 	AccessToken string `json:"access_token"`
+}
+
+func newJwtResponse(id uint) *jwtResponse {
+	r := new(jwtResponse)
+
+	r.AccessToken = utils.GenerateJWT(id)
+	return r
 }
