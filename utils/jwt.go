d4c0309612f70daf9181d4c765be992e3eb788a7
지농 <jinhong0719@naver.com>
2019-12-31 11:58:56 +0900

    jwt 토큰 생성 메서드 생성

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,1 +1,19 @@
 package utils
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var JWTSecret = []byte("SECRET_KEY")
+
+func GenerateJWT(id uint) string {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["id"] = id
+	claims["exp"] = time.Now().Add(time.Hour * 2).Unix()
+
+	t, _ := token.SignedString(JWTSecret)
+	return t
+}
