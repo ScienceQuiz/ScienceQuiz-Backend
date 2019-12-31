ee3c0a3c689d0dba060b454f464d4638508cf465
지농 <jinhong0719@naver.com>
2019-12-31 11:53:32 +0900

    미들웨어 Recover과 Logger 등록

--- router/router.go
+++ router/router.go
@@ -10,6 +10,9 @@ func New() *echo.Echo {
 	e := echo.New()
 
 	e.Validator = &CustomValidator{validator: validator.New()}
+
+	e.Use(echoMW.Logger())
+	e.Use(echoMW.Recover())
 	e.Use(echoMW.CORSWithConfig(echoMW.CORSConfig{
 		AllowOrigins:     []string{"*"},
 		AllowHeaders: 	  []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
