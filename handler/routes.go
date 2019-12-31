15ba9a457111bc6659fb5abab9b66aee0efe4aa2
지농 <jinhong0719@naver.com>
2019-12-31 14:44:32 +0900

    api/users/my API 라우팅

--- handler/routes.go
+++ handler/routes.go
@@ -5,8 +5,10 @@ import (
 )
 
 func (h *Handler) Register(v1 *echo.Group) {
-	guestUser := v1.Group("/users")
-
+	guestUser := v1.Group("")
 	guestUser.POST("/signup", h.SignUp)
 	guestUser.POST("/login", h.Login)
+
+	user := v1.Group("/users")
+	user.POST("/my", h.MyInform)
 }
