ad26d5df2b3ca7d50fda8d1c036692871d6ab3dc
지농 <jinhong0719@naver.com>
2020-01-03 10:30:22 +0900

    새 퀴즈 등록 API인 api/quiz POST API 로직 구현

--- handler/quiz.go
+++ handler/quiz.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"github.com/ScienceQuiz-Backend/model"
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func (h *Handler) QuizRegister(c echo.Context) error {
+	q := new(model.Quiz)
+	req := quizRegisterRequest{}
+
+	if err := req.bind(c, q); err != nil {
+		return err
+	}
+	if h.quizService.CheckQuizExist(q.Question) == true {
+		return echo.NewHTTPError(470, "이미 존재하는 질문의 문제입니다.")
+	}
+
+	h.quizService.Create(q)
+	return c.NoContent(http.StatusOK)
+}
